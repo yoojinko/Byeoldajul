d6ffadb29339f3e9c68378e13acd892906b955cb
yoojinko <db232100@gmail.com>
2022-08-29 15:17:00 +0900

    view 적용

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const path = require("path");
 const kogpt2Controller = require("./controller/kogpt2Controller.js");
 const app = express();
 const port = 3000;
 
 app.get("/", (req,res) => {
-    res.send("Hello World!"); 
+    res.render("index"); 
 })
 
 app.get("/guess", (req,res) => {
@@ -13,10 +14,13 @@ app.get("/guess", (req,res) => {
     
     const alphabets = word.split('');
 
-    kogpt2Controller.print(alphabets[1]);
+    kogpt2Controller.getSentence(alphabets[1]);
     res.send(alphabets[0]);
     //res.send('word');
 })
+
+app.set("views", path.join(__dirname,"src"));
+app.set("view engine", "ejs");
 
 app.listen(port, () => {
     console.log(`서버가 실행됩니다. http://localhost:${port}`)
--- controller/kogpt2Controller.js
+++ controller/kogpt2Controller.js
@@ -1,5 +1,37 @@
+const axios = require("axios");
+const FormData = require('form-data');
+
 module.exports = {
     print: function(a) {
         console.log(a);
-    }
+    },
+    
+    getSentence: function(str) {
+
+        var formData = new FormData();
+        formData.append('context',str);
+        formData.append('model','gpt2-story');
+        formData.append('length','long');
+
+        axios({
+            method: 'post',
+            url: 'https://kubecon-tabtab-ainize-team.endpoint.ainize.ai/gpt2',
+            headers: {
+                'Content-Type' : 'multipart/form-data'
+            },
+            data: {
+                context:str,
+                model: "gpt2-story",
+                length: "short"
+            }
+        })
+        .then((res) => {
+            console.log("in res");
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log("in err")
+            console.log(err);
+        })
+    } 
 }
