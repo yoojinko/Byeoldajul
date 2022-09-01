668a6cc6ba89a7dc6a7c0e78f8792d801ff6e540
yoojinko <db232100@gmail.com>
2022-09-02 02:18:42 +0900

    코드 정리

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const ejs = require("ejs");
 const kogpt2Controller = require("./controller/kogpt2Controller.js");
 const app = express();
 const port = 3000;
-const corsOptions= {
-    origin: 'http://localhost:3000'
-}
+// const corsOptions= {
+//     origin: 'http://localhost:3000'
+// }
 
 app.get("/", (req,res) => {
     res.render("index", {
@@ -19,7 +19,7 @@ app.get("/", (req,res) => {
 
 app.get("/guess", async (req,res) => {
     const org = req.query.word;
-    const size = req.query.size > 0 && req.query.size < 30 ? req.query.size : 16;
+    const size = req.query.size > 0 && req.query.size <= 30 ? req.query.size : 16;
     await kogpt2Controller.generateSentence(org, size).then((result)=>{
         
         res.render('index', {
@@ -35,7 +35,7 @@ app.get("/guess", async (req,res) => {
 
 app.set("views", path.join(__dirname,"src"));
 app.set("view engine", "ejs");
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 app.listen(port, () => {
     console.log(`서버가 실행됩니다. http://localhost:${port}`)
 })
--- controller/kogpt2Controller.js
+++ controller/kogpt2Controller.js
@@ -1,20 +1,17 @@
 const axios = require("axios");
 
 module.exports = {
-    print: function(a) {
-        console.log(a);
-    },
-    
     generateSentence: async function(str, size) {
         const alphabets = str.split('');
 
-        console.log(str);
         let resString = '';
         let paramString = '';
         const sentSize = [];
         sentSize.push(-1);
+
         for(const alph of alphabets) {
             if(alph === ' ') continue;
+
             paramString = resString + " " + alph;
             await this.getSentenceByAlph(paramString, size).then((res)=>{
                 resString = res;
@@ -24,26 +21,23 @@ module.exports = {
                 console.log(err);
             })
         }
-        console.log(sentSize)
+        
         const sentences = [];
         for(var i = 1; i <= sentSize.length; i++) {
             sentences.push(resString.substr(sentSize[i-1]+1, sentSize[i] - sentSize[i-1]))
         }
-        console.log(sentences);
+        
         let result = '';
         for(const sentence of sentences) {
             let first = '<b>'+sentence.substr(0,1)+'</b>';
             let elseStr = sentence.substr(1, sentence.length-1)+'<br>';
             result = result+first+elseStr;
-
-            console.log(resString);
         }
 
         return '<p>'+result+'</p>';
     },
 
     getSentenceByAlph: async function(str, size) {
-        console.log("start", str, size);
         const result = await axios({
             method: 'post',
             url: 'https://skt-kogpt2-text-generation-ainize-team.endpoint.ainize.ai/prediction/generate',
@@ -62,8 +56,6 @@ module.exports = {
         .catch((err) => {
             console.log(err);
         })
-        console.log('end');
-        console.log(result);
         return result;
     } 
 }
