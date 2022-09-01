737739580d07211d5bee1d765b400d85970bac39
yoojinko <db232100@gmail.com>
2022-09-01 12:39:03 +0900

    결과 문장 분할 방식 변경

--- controller/kogpt2Controller.js
+++ controller/kogpt2Controller.js
@@ -11,19 +11,25 @@ module.exports = {
         console.log(str);
         let resString = '';
         let paramString = '';
-    
+        const sentSize = [];
+        sentSize.push(-1);
         for(const alph of alphabets) {
             if(alph === ' ') continue;
-            paramString = resString + "/" + alph;
+            paramString = resString + " " + alph;
             await this.getSentenceByAlph(paramString, size).then((res)=>{
                 resString = res;
-            })
+                sentSize.push(resString.length)
+        })
             .catch((err) => {
                 console.log(err);
             })
         }
-
-        const sentences = resString.split('/');
+        console.log(sentSize)
+        const sentences = [];
+        for(var i = 1; i <= sentSize.length; i++) {
+            sentences.push(resString.substr(sentSize[i-1]+1, sentSize[i] - sentSize[i-1]))
+        }
+        console.log(sentences);
         let result = '';
         for(const sentence of sentences) {
             let first = '<b>'+sentence.substr(0,1)+'</b>';
